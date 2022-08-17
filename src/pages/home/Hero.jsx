17b6c41346f6b83f68b7c931b3e35c773f732075
ujjwal6792 <ujjwal.sharma6792@gmail.com>
2022-08-18 00:03:20 +0530

    home vertical fix

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -2,7 +2,7 @@ import HomeButton from "../../components/buttons/HomeButton";
 import content from "../../content";
 
 const Hero = () =>
-    <main className="flex flex-col min-h-full">
+    <main className="flex flex-col min-h-full max-h-[700px] ">
           <section className="md:w-2/3 lg:w-1/2  min-h-[70vh] flex flex-col p-2 md:pt-[10%] justify-center bg-white leading-8">
             <img
               className="w-64 mx-auto sm:ml-[18%] mb-6"
