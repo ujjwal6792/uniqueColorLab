d3b66f49b58f655b0dd6ef37a0d348ac3e7b4465
ujjwal6792 <ujjwal.sharma6792@gmail.com>
2022-08-19 11:03:56 +0530

    completed contact form frontend in contact.jsx

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,10 +1,12 @@
+import { useRef } from "react";
 import { FiPhoneCall } from "react-icons/fi";
 import Cardsquare from "../../components/cards/Cardsquare";
 import { GoLocation, GoMail, GoDeviceMobile } from "react-icons/go";
-import { FaFacebookSquare,FaInstagram, FaTwitterSquare } from "react-icons/fa";
+import { FaFacebookSquare, FaInstagram, FaTwitterSquare } from "react-icons/fa";
 import content from "../../content";
 import maps from "./map.svg";
 import DottedMap from "dotted-map";
+import emailjs from "@emailjs/browser";
 
 const map = new DottedMap({ height: 80, grid: "diagonal" });
 
@@ -20,6 +22,28 @@ const Contact = () => {
     shape: "circle",
     backgroundColor: "#ffffff",
   });
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <div className="md:pt-24 flex flex-col">
@@ -72,40 +96,70 @@ const Contact = () => {
       </section>
       <div className="grid grid-cols-1 md:grid-cols-2 -mt-96 z-10 justify-between items-center">
         {/* contact form */}
-        <section>form</section>
+
+        <section className="bg-white shadow-md justify-center items-center h-full w-[80%] px-8 mx-auto py-8 rounded-md">
+          <form className="flex flex-col text-blue-dark " ref={form} onSubmit={sendEmail}>
+            <label className="sm:text-lg">Name</label>
+            <input
+              className="sm:text-lg border-2 text-lg py-1 px-2 border-blue-dark rounded-md focus:outline-0 text-blue-dark shadow"
+              type="text"
+              name="user_name"
+            />
+            <label className="sm:text-lg">Email</label>
+            <input
+              className="sm:text-lg border-2 text-lg py-1 px-2 border-blue-dark rounded-md focus:outline-0 text-blue-dark shadow"
+              type="email"
+              name="user_email" 
+            />
+            <label className="sm:text-lg">Your Message</label>
+            <textarea
+              className="sm:text-lg border-2 text-lg py-1 px-2 border-blue-dark rounded-md focus:outline-0 text-blue-dark shadow"
+              name="message"
+            />
+            <input
+              className="mt-2 text-lg h-min w-max border-blue-900 hover:bg-blue-900 hover:text-white hover:transition-opacity shadow-md rounded-md px-2 py-1 text-blue-dark border-2"
+              type="submit"
+              value="Send"
+            />
+          </form>
+        </section>
+
         <div>
-        {/* opening hours */}
-        <section className="flex flex-col justify-center items-center bg-white h-full w-[80%] shadow-md px-8 mx-auto py-8 rounded-t-md">
-          <h1 className="font-semibold px-2 text-blue-dark mb-2">
-            Opening Hours
-          </h1>
-          <h1 className="font-semibold px-2 text-2xl text-blue-dark mb-4">
-            We Are Open
-          </h1>
-          <ul className="px-2 sm:px-24 md:px-4 lg:px-20 grid gap-2  w-full">
-            <li className="flex justify-between">
-              <span className="sm:text-lg">Weekdays</span>
-              <span className="font-semibold">09:00-22:00</span>
-            </li>
-            <li className="flex justify-between">
-              <span className="sm:text-lg">Saturday</span>
-              <span className="font-semibold">09:00-23:00</span>
-            </li>
-            <li className="flex justify-between">
-              <span className="sm:text-lg">Sunday</span>
-              <span className="font-semibold">Closed</span>
-            </li>
-          </ul>
-        </section>
-        {/* socials */}
-        <section className="bg-blue-dark text-xl text-white px-8 mx-auto py-8 w-[80%] rounded-b-md flex justify-between items-center">
-        <span>Follow Us On</span>
-        <span className="flex gap-4">
-            <FaFacebookSquare/>
-            <FaInstagram/>
-            <FaTwitterSquare/>
-        </span>
-        </section>
+          {/* opening hours */}
+
+          <section className="flex flex-col justify-center items-center bg-white h-full w-[80%] shadow-md px-8 mx-auto py-8 rounded-t-md">
+            <h1 className="font-semibold px-2 text-blue-dark mb-2">
+              Opening Hours
+            </h1>
+            <h1 className="font-semibold px-2 text-2xl text-blue-dark mb-4">
+              We Are Open
+            </h1>
+            <ul className="px-2 sm:px-24 md:px-4 lg:px-20 grid gap-2  w-full">
+              <li className="flex justify-between">
+                <span className="sm:text-lg">Weekdays</span>
+                <span className="font-semibold">09:00-22:00</span>
+              </li>
+              <li className="flex justify-between">
+                <span className="sm:text-lg">Saturday</span>
+                <span className="font-semibold">09:00-23:00</span>
+              </li>
+              <li className="flex justify-between">
+                <span className="sm:text-lg">Sunday</span>
+                <span className="font-semibold">Closed</span>
+              </li>
+            </ul>
+          </section>
+
+          {/* socials */}
+
+          <section className="bg-blue-dark text-xl text-white px-8 mx-auto py-8 w-[80%] rounded-b-md flex justify-between items-center">
+            <span>Follow Us On</span>
+            <span className="flex gap-4">
+              <FaFacebookSquare />
+              <FaInstagram />
+              <FaTwitterSquare />
+            </span>
+          </section>
         </div>
       </div>
     </div>
