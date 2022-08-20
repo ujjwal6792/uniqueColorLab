beaffb66aaec921e490a2fad51117bc92ee36f83
ujjwal6792 <ujjwal.sharma6792@gmail.com>
2022-08-21 02:30:43 +0530

    added reset password

--- src/pages/credentials/Login.jsx
+++ src/pages/credentials/Login.jsx
@@ -1,29 +1,32 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
 import TextFieldBase from "../../components/textFields/TextFieldBase";
 import HomeButton from "../../components/buttons/HomeButton";
 
 const Login = () => {
-    const [formValues, setFormValues] = useState()
+  const [formValues, setFormValues] = useState();
+  const Navigate = useNavigate();
 
-    const validate = Yup.object({
-        email:  Yup.string().email( "Email is invalid").required("Email is required"),
-        password:  Yup.string().required("Password is required"),
-    })
+  const validate = Yup.object({
+    email: Yup.string().email("Email is invalid").required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
   return (
     <Formik
-    initialValues={{
-      email: "",
-      password: "",
-    }}
-    validationSchema={validate}
-    onSubmit = {values => {setFormValues(values)}}
-  >
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={validate}
+      onSubmit={(values) => {
+        setFormValues(values);
+      }}
+    >
       <div className="pt-20 px-6 md:px-8 md:pt24">
-      <h1 className="text-5xl text-blue-dark pb-10 md:pd-20"> Login</h1>
+        <h1 className="text-5xl text-blue-dark pb-10 md:pd-20"> Login</h1>
         <Form className="flex flex-col gap-4">
-
           <TextFieldBase
             style="min-w-[300px] max-w-[400px] w-[40vw] text-blue-dark"
             placeholder="Email Address"
@@ -39,12 +42,20 @@ const Login = () => {
             name="password"
             labelstyle="font-semibold -my-2 text-blue-dark"
           />
-
+          <p onClick={()=>Navigate("/change_password")} className="text-blue-dark -mb-4 cursor-pointer">forgot your password?</p>
           <div className="flex gap-4 -mt-2">
-              <HomeButton type="submit" text="Login" style="rounded-md text-blue-dark"/>
-              <HomeButton type="reset" text="Reset" style="rounded-md border-red-600 text-red-600 hover:bg-red-600"/>
+            <HomeButton
+              type="submit"
+              text="Login"
+              style="rounded-md text-blue-dark"
+            />
+            <HomeButton
+              function={() => Navigate("/register")}
+              text="register"
+              style="rounded-md"
+            />
           </div>
-         </Form>
+        </Form>
       </div>
     </Formik>
   );
