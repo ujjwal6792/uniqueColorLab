cbdb27c30691f50c1a98f5e97c17f553b31b0308
ujjwal6792 <ujjwal.sharma6792@gmail.com>
2022-08-21 01:42:57 +0530

    added yup validation to register page

--- src/components/textFields/TextFieldBase.jsx
+++ src/components/textFields/TextFieldBase.jsx
@@ -1,14 +1,16 @@
 // type, style, placeholder, name, labelstyle
-import { useField } from "formik";
+import { useField, ErrorMessage } from "formik";
 
 const TextFieldBase = ({style,placeholder,labelstyle, ...props}) => {
   const [field, meta] = useField(props);
   return (<>
-  {}
-    <label htmlFor={field.name} className={labelstyle}>{placeholder}</label>
+    <div className="flex gap-2 items-center">
+        <label htmlFor={field.name} className={labelstyle}>{placeholder}</label>
+        <ErrorMessage component="div" className="text-red-600" name = {field.name}/>
+    </div>
     <input
       {...field} {...props}
-      className={`py-1 px-2 border-2 rounded-md border-blue-dark outline-none ${style}`}
+      className={`py-1 px-2 border-2 rounded-md border-blue-dark outline-none ${style} ${meta.touched&&meta.error&& "border-red-600 animate-pulse"}`}
       placeholder={placeholder}
       name={field.name}
     />
--- src/pages/credentials/Register.jsx
+++ src/pages/credentials/Register.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import {useState,useEffect} from "react";
 import { Form, Formik } from "formik";
+import * as Yup from "yup"
 import TextFieldBase from "../../components/textFields/TextFieldBase";
 import HomeButton from "../../components/buttons/HomeButton";
 
 const Register = () => {
+
+    const [formValues, setFormValues] = useState()
+
+    const validate = Yup.object({
+        firstName: Yup.string().max(20, "Must be 20 letters or less").required("Required"),
+        lastName:  Yup.string().max(20, "Must be 20 letters or less").required("Required"),
+        email:  Yup.string().email( "Email is invalid").required("Email is required"),
+        password:  Yup.string().min(8, "Must be 8 characters or more").required("Password is required"),
+        confirmPassword:  Yup.string().oneOf([Yup.ref("password"), null], "Password much match").required("Confirm password is required"), 
+    })
   return (
     <Formik
       initialValues={{
@@ -13,12 +24,13 @@ const Register = () => {
         password: "",
         confirmPassword: "",
       }}
+      validationSchema={validate}
+      onSubmit = {values => {setFormValues(values)}}
     >
         {formik => (
       <div className=" pt-20 px-6 md:px-8 md:pt24">
         <h1 className="text-5xl text-blue-dark pb-10 md:pd-20"> Register</h1>
         <Form className="flex flex-col gap-4">
-
           <TextFieldBase
             style="min-w-[300px] max-w-[400px] w-[40vw] text-blue-dark"
             placeholder="First Name"
@@ -58,7 +70,7 @@ const Register = () => {
           />
           <div className="flex gap-4 -mt-2">
               <HomeButton type="submit" text="Register" style="rounded-md text-blue-dark"/>
-              <HomeButton type="submit" text="Reset" style="rounded-md border-red-500 text-red-500 hover:bg-red-500"/>
+              <HomeButton type="reset" text="Reset" style="rounded-md border-red-600 text-red-600 hover:bg-red-600"/>
           </div>
          </Form>
       </div>
