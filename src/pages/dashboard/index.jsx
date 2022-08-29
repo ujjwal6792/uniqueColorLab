9a6d2b16787fc4e8e0dd33c91e6aa2c69ab1a4a9
ujjwal6792 <ujjwal.sharma6792@gmail.com>
2022-08-29 22:15:56 +0530

    added dashboard grid

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -14,10 +14,37 @@ const Dashboard = () => {
   } 
   return ( <>
     { isLoggedIn===true?
-    <div className=' pt-20'>
-      <p>{(user?.firstname)}</p>
-    <button onClick={()=> logOut()}
-    className="rounded-md border-red-600 text-red-600 hover:bg-red-600 mt-8 text-lg border h-min w-max py-2 px-4 hover:text-white hover:transition-opacity shadow-md">logout</button>
+    <div className='pt-20 grid grid-cols-12 text-blue-dark'>
+
+    <div className='flex flex-col gap-4 p-4 col-span-3 '>
+      <section className="flex gap-4 items-center shadow-md p-2">
+        {user?.img===undefined?
+        <img className="border rounded-full p-1 w-16" src="https://img.icons8.com/laces/64/000000/user.png" alt="" />
+        :
+        <img src={user?.img} alt="" /> }
+        
+        <p className="text-2xl font-semibold">{`Hi, ${user?.firstname}`}</p>
+      </section>
+      <section className="flex flex-col gap-2 items-center shadow-md py-2">
+       <p className="text-xl px-2 pb-2 border-b-2 w-full font-semibold">My Orders</p>
+       <p className="text-xl px-2 pb-2 w-full font-semibold">Account Details
+       <p className="pl-8 pt-4 pb-2 text-base">Personal Details</p>
+       <p className="pl-8 py-2 text-base">Manage Addresses</p>
+       <p className="pl-8 py-2 text-base">My Wishlist</p>
+
+       </p>
+      </section>
+      <section className="flex flex-col gap-2 items-center shadow-md py-2 bg-blue-dark">
+      <button onClick={()=> logOut()} className="text-xl text-white font-semibold text-opacity-95">Logout</button>
+      </section>
+    </div>
+
+
+    <div className="col-span-9 p-4 h-full ">
+      <section className="h-full shadow-md">
+        
+      </section>
+    </div>
     </div>
     : Navigate('/login')
   }
